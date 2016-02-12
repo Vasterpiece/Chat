30a7e743883d1b8a288c6a7e8652dca6998c8cd4
Vasterpiece <DenisLutsenko96@gmail.com>
2016-02-12 08:09:06 +0200

    task #10

--- src/Chat/Server.java
+++ src/Chat/Server.java
@@ -25,14 +25,24 @@ public class Server {
         }
     }
 
+    private void serverMainLoop(Connection connection, String userName) throws IOException, ClassNotFoundException {
+        while (true) {
+            Message message = connection.receive();
+            if (message.getType() == MessageType.TEXT)
+                sendBroadcastMessage(new Message(message.getType(), userName + ": " + message.getData()));
+            else
+                ConsoleHelper.writeMessage("Ошибка!");
+        }
+    }
     private static class Handler extends Thread
     {
         private Socket socket;
-        public Handler(Socket socket) {
+
+        private Handler(Socket socket) {
             this.socket = socket;
         }
 
-        String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+        private String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
             String name;
             while (true) {
                 connection.send(new Message(MessageType.NAME_REQUEST));
@@ -53,6 +63,20 @@ public class Server {
                 break;
             }
             return name;
+        }
+
+        private void sendListOfUsers(Connection connection, String userName) throws IOException {
+            for (String s : connectionMap.keySet()
+                    ) {
+                if (s.equals(userName))
+                    continue;
+                connection.send(new Message(MessageType.USER_ADDED, s));
+            }
+        }
+
+        @Override
+        public void run() {
+            super.run();
         }
     }
 
@@ -75,3 +99,27 @@ public class Server {
     }
 }
 
+/*		Пришло время написать главный метод класса Handler, который будет вызывать все
+вспомогательные методы, написанные ранее. Добавим метод void run() в класс Handler.
+Он должен:
+11.1.	Выводить сообщение, что установлено новое соединение с удаленным
+адресом, который можно получить с помощью метода getRemoteSocketAddress
+11.2.	Создавать Connection, используя поле Socket
+11.3.	Вызывать метод, реализующий рукопожатие с клиентом, сохраняя имя нового
+клиента
+11.4.	Рассылать всем участникам чата информацию об имени присоединившегося
+участника (сообщение с типом USER_ADDED). Подумай, какой метод подойдет для
+этого лучше всего.
+11.5.	Сообщать новому участнику о существующих участниках
+11.6.	Запускать главный цикл обработки сообщений сервером
+11.7.	Обеспечить закрытие соединения при возникновении исключения
+11.8.	Отловить все исключения типа IOException и ClassNotFoundException, вывести в
+консоль информацию, что произошла ошибка при обмене данными с удаленным
+адресом
+11.9.	После того как все исключения обработаны, если п.11.3 отработал и возвратил
+нам имя, мы должны удалить запись для этого имени из connectionMap и разослать
+всем остальным участникам сообщение с типом USER_REMOVED и сохраненным
+именем.
+11.10.	Последнее, что нужно сделать в методе run() – вывести сообщение,
+информирующее что соединение с удаленным адресом закрыто.
+Наш сервер полностью готов. Попробуй его запустить.*/
