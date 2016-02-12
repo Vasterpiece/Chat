0fe0bb2250053077fe076d4037f68b920518e1fb
Vasterpiece <DenisLutsenko96@gmail.com>
2016-02-12 07:43:57 +0200

    Added "ServerHandShake" method

--- src/Chat/Server.java
+++ src/Chat/Server.java
@@ -7,9 +7,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 
-/**
- * Created by Login on 10.02.2016.
- */
+
 public class Server {
     private static Map<String,Connection> connectionMap = new ConcurrentHashMap<String, Connection>();
 
@@ -30,9 +28,31 @@ public class Server {
     private static class Handler extends Thread
     {
         private Socket socket;
-
         public Handler(Socket socket) {
             this.socket = socket;
+        }
+
+        String serverHandshake(Connection connection) throws IOException, ClassNotFoundException {
+            String name;
+            while (true) {
+                connection.send(new Message(MessageType.NAME_REQUEST));
+                Message message = connection.receive();
+                if (message.getType() != MessageType.USER_NAME)
+                    continue;
+
+                name = message.getData();
+                if (name == null || name.isEmpty())
+                    continue;
+                if (connectionMap.containsKey(name)) {
+                    connection.send(new Message(MessageType.TEXT, "Пользователь с таким именем уже существует!"));
+                    continue;
+                }
+
+                connectionMap.put(name, connection);
+                connection.send(new Message(MessageType.NAME_ACCEPTED));
+                break;
+            }
+            return name;
         }
     }
 
@@ -53,18 +73,5 @@ public class Server {
             ConsoleHelper.writeMessage("Ошибка запуска сервера!");
         }
     }
-
 }
 
-/*		Т.к. сервер может одновременно работать с несколькими клиентами, нам понадобится
-метод для отправки сообщения сразу всем.
-Добавь в класс Server:
-7.1.	Статическое поле Map<String, Connection> connectionMap, где ключом будет имя
-клиента, а значением - соединение с ним.
-7.2.	Инициализацию поля из п.7.1 с помощью подходящего Map из библиотеки
-java.util.concurrent, т.к. работа с этим полем будет происходить из разных потоков и
-нужно обеспечить потокобезопасность.
-7.3.	Статический метод void sendBroadcastMessage(Message message), который должен
-отправлять сообщение  message по всем соединениям из connectionMap. Если при
-отправке сообщение произойдет исключение IOException, нужно отловить его и
-сообщить пользователю, что не смогли отправить сообщение.*/
