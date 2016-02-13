843aaa6ead7c641730f458696897b3b6eb068a6f
Vasterpiece <DenisLutsenko96@gmail.com>
2016-02-13 08:52:38 +0200

    Some client methods added

--- src/Chat/client/Client.java
+++ src/Chat/client/Client.java
@@ -6,10 +6,16 @@ import Chat.Message;
 import Chat.MessageType;
 
 import java.io.IOException;
+import java.net.Socket;
 
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected = false;
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 
     protected String getServerAddress() {
         ConsoleHelper.writeMessage("Введите адрес сервера.");
@@ -17,7 +23,65 @@ public class Client {
     }
 
     public class SocketThread extends Thread {
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+        }
 
+        protected void informAboutAddingNewUser(String userName) {
+            ConsoleHelper.writeMessage("Пользователь: " + userName + ". Присоединилс к чату.");
+        }
+
+        protected void informAboutDeletingNewUser(String userName) {
+            ConsoleHelper.writeMessage("Пользователь: " + userName + " покинул чат.");
+        }
+
+        protected void notifyConnectionStatusChanged(boolean clientConnected) {
+            Client.this.clientConnected = clientConnected;
+            synchronized (Client.this) {
+                Client.this.notify();
+            }
+        }
+
+        protected void clientHandshake() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = Client.this.connection.receive();
+                if (message.getType() == MessageType.NAME_REQUEST)
+                    connection.send(new Message(MessageType.USER_NAME, getUserName()));
+                else if (message.getType() == MessageType.NAME_ACCEPTED) {
+                    notifyConnectionStatusChanged(true);
+                    break;
+                } else
+                    throw new IOException("Unexpected MessageType");
+            }
+        }
+
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            while (true) {
+                Message message = connection.receive();
+                if (message.getType() == MessageType.TEXT)
+                    processIncomingMessage(message.getData());
+                else if (message.getType() == MessageType.USER_ADDED)
+                    informAboutAddingNewUser(message.getData());
+                else if (message.getType() == MessageType.USER_REMOVED)
+                    informAboutDeletingNewUser(message.getData());
+                else
+                    throw new IOException("Unexpected MessageType");
+            }
+        }
+
+        @Override
+        public void run() {
+            String serverAddress = getServerAddress();
+            int portNumber = getServerPort();
+            try (Socket socket = new Socket(serverAddress, portNumber)) {
+                Client.this.connection = new Connection(socket);
+                clientHandshake();
+                clientMainLoop();
+            } catch (Exception e) {
+                notifyConnectionStatusChanged(false);
+            }
+
+        }
     }
 
     protected int getServerPort() {
@@ -48,32 +112,43 @@ public class Client {
     }
 
     public void run() {
-
+        SocketThread socketThread = getSocketThread();
+        socketThread.setDaemon(true);
+        socketThread.start();
+        synchronized (this) {
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                System.out.println("Произошла ощибка при вызове метода wait");
+                System.exit(-1);
+            }
+        }
+        if (clientConnected)
+            ConsoleHelper.writeMessage("Соединение установлено. Для выхода наберите команду 'exit'.");
+        else
+            ConsoleHelper.writeMessage("Произошла ошибка во время работы клиента.");
+        while (clientConnected) {
+            String s = ConsoleHelper.readString();
+            if (s.equals("exit"))
+                break;
+            if (shouldSentTextFromConsole())
+                sendTextMessage(s);
+        }
     }
 }
-/*Приступим к написанию главного функционала класса Client.
-14.1.	Добавь метод void run(). Он должен создавать вспомогательный поток
-SocketThread, ожидать пока тот установит соединение с сервером, а после этого
-в цикле считывать сообщения с консоли и отправлять их серверу. Условием выхода
-из цикла будет отключение клиента или ввод пользователем команды 'exit'.
-Для информирования главного потока, что соединение установлено во
-вспомогательным потоке, используй методы wait и notify объекта класса Client.
-Реализация метода run должна:
-14.1.1.	Создавать новый сокетный поток с помощью метода getSocketThread.
-14.1.2.	Помечать созданный поток как daemon, это нужно для того, чтобы при выходе
-из программы вспомогательный поток прервался автоматически.
-14.1.3.	Запустить вспомогательный поток.
-14.1.4.	Заставить текущий поток ожидать, пока он не получит нотификацию из другого
-потока. Подсказка: используй wait и синхронизацию на уровне объекта. Если во
-время ожидания возникнет исключение, сообщи об этом пользователю и выйди
-из программы.
-14.1.5.	После того, как поток дождался нотификации, проверь значение
-clientConnected. Если оно true – выведи "Соединение установлено. Для выхода
-наберите команду 'exit'.". Если оно false – выведи "Произошла ошибка во время
-работы клиента.".
-14.1.6.	Считывай сообщения с консоли пока клиент подключен. Если будет введена
-команда 'exit', то выйди из цикла.
-14.1.7.	После каждого считывания, если метод shouldSentTextFromConsole()
-возвращает true, отправь считанный текст с помощью метода  sendTextMessage().
-14.2.	Добавь метод main(). Он должен создавать новый объект класса Client и
-вызывать у него метод run().*/
+/*
+    Последний, но самый главный метод класса SocketThread – это метод void run(). Добавь
+его. Его реализация с учетом уже созданных методов выглядит очень просто. Давай
+напишем ее:
+17.1.	Запроси адрес и порт сервера с помощью методов getServerAddress() и
+getServerPort().
+17.2.	Создай новый объект класса java.net.Socket, используя данные, полученные в
+п.17.1.
+17.3.	Создай объект класса Connection, используя сокет из п.17.2.
+17.4.	Вызови метод, реализующий "рукопожатие" клиента с сервером
+(clientHandshake()).
+17.5.	Вызови метод, реализующий основной цикл обработки сообщений сервера.
+17.6.	При возникновении исключений IOException или ClassNotFoundException
+сообщи главному потоку о проблеме, используя notifyConnectionStatusChanged и false
+в качестве параметра.
+Клиент готов, можешь запустить сервер, несколько клиентов и проверить как все работает.*/
